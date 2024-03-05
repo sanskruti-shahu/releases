e850ed6d329d702afa5fd061c88c02eacd30363f
sanskruti-shahu <sanskruti.shahu@harness.io>
2024-03-05 17:39:34 +0530

    Update main.go

--- main.go
+++ main.go
@@ -34,6 +34,10 @@ const (
 )
 
 func main() {
+	os.Exit(realMain())
+}
+
+func realMain() int {
 	const (
 		githubAPIEndpoint = "https://api.github.com/repos/opentofu/opentofu/releases"
 		htmlFileName      = "index.html"
@@ -44,7 +48,7 @@ func main() {
 	req, err := http.NewRequest("GET", githubAPIEndpoint, nil)
 	if err != nil {
 		fmt.Println("Error creating request: ", err)
-		return
+		return 1
 	}
 	req.Header.Set("User-Agent", "opentofu releases page")
 
@@ -61,28 +65,28 @@ func main() {
 	response, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error fetching releases: ", err)
-		return
+		return 1
 	}
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println("Error reading response body: ", err)
-		return
+		return 1
 	}
 
 	// Unmarshal JSON response
 	releases := []map[string]interface{}{}
 	if err := json.Unmarshal(body, &releases); err != nil {
 		fmt.Println("Error unmarshalling JSON: ", err)
-		return
+		return 1
 	}
 
 	// Create main index.html file
 	htmlFile, err := os.Create(htmlFileName)
 	if err != nil {
 		fmt.Println("Error creating main HTML file: ", err)
-		return
+		return 1
 	}
 
 	// Function to remove the first character of a string
@@ -99,26 +103,26 @@ func main() {
 	mainPageTmpl, err := template.New("mainPageTemplate").Funcs(funcMap).Parse(mainPageTemplate)
 	if err != nil {
 		fmt.Println("Error parsing mainPage template: ", err)
-		return
+		return 1
 	}
 
 	// Execute the mainPage template and write to the main index.html
 	if err := mainPageTmpl.Execute(htmlFile, releases); err != nil {
 		fmt.Println("Error executing mainPage template: ", err)
-		return
+		return 1
 	}
 
 	// Handling error while closing main index.html file
 	if err := htmlFile.Close(); err != nil {
 		fmt.Println("Error closing main HTML file: ", err)
-		return
+		return 1
 	}
 
 	// Parse releasePage template
 	releasePageTmpl, err := template.New("releasePageTemplate").Parse(releasePageTemplate)
 	if err != nil {
 		fmt.Println("Error parsing releasePage template: ", err)
-		return
+		return 1
 	}
 
 	// Iterate over releases to create release pages
@@ -130,28 +134,30 @@ func main() {
 		// Create directory for each release
 		if err := os.Mkdir(path, 0755); err != nil && !os.IsExist(err) {
 			fmt.Println(fmt.Sprintf("Error creating %s directory: ", version), err)
-			return
+			return 1
 		}
 
 		// Create index.html file for release page
 		htmlFile, err := os.Create(path + htmlFileName)
 		if err != nil {
 			fmt.Println(fmt.Sprintf("Error creating HTML file, %s%s: ", path, htmlFileName), err)
-			return
+			return 1
 		}
 
 		// Execute releasePage template and write to releases' index.html
 		if assets, ok := release["assets"].([]interface{}); ok {
 			if err := releasePageTmpl.Execute(htmlFile, assets); err != nil {
 				fmt.Println(fmt.Sprintf("Error executing releasePage template for %s%s: ", path, htmlFileName), err)
-				return
+				return 1
 			}
 		}
 
 		// Handling error while closing releases' index.html file
 		if err := htmlFile.Close(); err != nil {
 			fmt.Println(fmt.Sprintf("Error closing HTML file, %s%s: ", path, htmlFileName), err)
-			return
+			return 1
 		}
 	}
+
+	return 0
 }
